dca00727c07f0fc95610b7915caea317c878acd6
Rajab Imam <rajabimam@yahoo.com>
2022-01-17 17:53:57 +0100

    cors

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const mysql = require('mysql')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(cors())
-app.options('*', cors())
+app.use(cors({
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}))
+//app.options('', cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
