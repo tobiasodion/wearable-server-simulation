5ef97197294ed69fb9399e0fec6092d7edace5a3
Rajab Imam <rajabimam@yahoo.com>
2022-01-17 17:09:12 +0100

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 var mysql = require('mysql')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
 
 /* Production */
 var connection = mysql.createConnection({
