580f82a3b3c4d3a5f779961299a0a96eb015a67f
Rajab Imam <rajabimam@yahoo.com>
2022-01-17 17:40:28 +0100

    done

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const mysql = require('mysql')
 const bodyParser = require('body-parser')
 
 const app = express()
-app.use(cors())
+app.use(cors(builder =>
+  builder.WithOrigins("https://wearable-device.herokuapp.com")
+  .AllowAnyHeader()
+  .AllowCredentials()
+  .AllowAnyMethod()))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
